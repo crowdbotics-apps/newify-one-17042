bb2001450300da4638101c750b29c2906f3f192f
Crowdbotics <team@crowdbotics.com>
2020-05-15 13:20:20 +0000

    Update models

--- newify_one_17042/wsgi.py
+++ newify_one_17042/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newify_one_17042.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newify_one_17042.settings")
 
 application = get_wsgi_application()
